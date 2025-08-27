f0ac494ecdc5f2970b885873a6ac2cfe90c3def6
shemzoo <lvn-gg@yandex.ru>
2025-08-27 22:22:52 +0300

    импорт svg теперь через ?react

--- src/pages/CharacterInfo/CharacterInfo.component.tsx
+++ src/pages/CharacterInfo/CharacterInfo.component.tsx
@@ -1,20 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import ArrowBackIcon from "@/components/ArrowBackIcon/ArrowBackIcon.component";
-import Loader from "@/components/Loader/Loader.component";
+import ArrowBackIcon from '@/assets/arrow-back.svg?react';
+import Loader from '@/components/Loader/Loader.component';
 
-import styles from "./CharacterInfo.module.scss";
+import styles from './CharacterInfo.module.scss';
 
 const CharacterInfo = () => {
   return (
     <div className={styles.characterInfo}>
-      <Link to="/" className={styles.characterInfo__goBack}>
+      <Link
+        to='/'
+        className={styles.characterInfo__goBack}
+      >
         <ArrowBackIcon />
         <span>GO BACK</span>
       </Link>
-      <Loader 
-        size="large"
-        text="Loading character card..." />
+      <Loader
+        size='large'
+        text='Loading character card...'
+      />
     </div>
   );
 };
