3c9d653cf114a7513edb121f803bd1851c5e87cb
shemzoo <lvn-gg@yandex.ru>
2025-11-12 12:25:32 +0300

    подготовка к деплою

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/rick-and-morty-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
