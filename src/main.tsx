bc47098b111fa07d8076c4d3984b629894f40c9a
shemzoo <lvn-gg@yandex.ru>
2025-11-12 12:32:05 +0300

    добавлен basename

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/rick-and-morty-app'>
       <Toaster position='bottom-right' />
       <App />
     </BrowserRouter>
