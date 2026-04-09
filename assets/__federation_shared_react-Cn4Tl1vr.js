853116f3f84aca79a1638dccb3c83d73e457741d
shemzoo <lvn-gg@yandex.ru>
2026-04-09 11:07:43 +0300

    Updates

---
+++ assets/__federation_shared_react-Cn4Tl1vr.js
@@ -0,0 +1,1 @@
+import{r,g as t}from"./index-TjqCB-xA.js";var e=r();const o=t(e);export{o as default};
---
+++ assets/__federation_shared_react-dom-CzB2RsCX.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index-TjqCB-xA.js";import{r as o}from"./index-PwaJdsPd.js";var t=o();const m=r(t);export{m as default};
